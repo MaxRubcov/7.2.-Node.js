5dfd5693b14bda8aa7a43c7cb2ad94933deaf754
MaxRU <m_rubcov@bk.ru>
2024-08-13 22:39:35 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const func = () => {
+  const sum = (a, b) => a + b
+  let x = 103;
+  let y = 20;
+
+  console.log(sum(x, y))
+  
+  debugger
+  x = 20
+  y = 30
+
+  console.log(sum(x, y))
+};
+
+func()
